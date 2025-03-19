e4bbeea35195edf2aba9a8f94e52a326161d8a74
gitjnichols3 <j_nichols2@mail.fhsu.edu>
2025-03-19 12:00:05 -0700

    sdfasdfsdf

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -43,9 +43,9 @@
             // Push to "data"
             array_push($quotes_array['data'], $quote_item);
         }
-
+        $singleObject = $quotes_array[0];
         //Turn to JSON & output
-        echo json_encode($quotes_array['data']);
+        echo json_encode($singleObject);
 
     }else{
         // No quotes
