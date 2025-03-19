ec96e9c54394490548f8bc74384502bb4e3bc23e
gitjnichols3 <j_nichols2@mail.fhsu.edu>
2025-03-19 11:40:17 -0700

    try from quote

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -20,17 +20,6 @@
     // Get post
     $quote->read_single();
     
-            //Create array
-            $quote_arr = array(
-                'id' => $quote->id,
-                'quote' => $quote->quote,
-            // 'author_id' => $quote->author_id,
-                'author' => $quote->author,
-            // 'category_id' => $quote->category_id,
-                'category' => $quote->category
-            );
 
-    //Make JSON
-    print_r(json_encode($quote_arr));
     
 
